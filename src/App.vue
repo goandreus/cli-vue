51b7a472e6dc2bb27511bc725183e51dfa9aafa6
goandreus <andreshtml5@gmail.com>
2019-08-02 11:27:07 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,21 @@
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+  
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
+        <span class="mr-2">Cerrar sesion</span>
+        <v-icon>fas fa-address-book</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container>
+        <router-view/>
+      </v-container>
+    
     </v-content>
   </v-app>
 </template>
